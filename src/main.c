b37ecdf90c632d844751a04f986dca318969ffc2
ibuki2003 <main@fuwa.dev>
2023-10-26 23:25:06 +0900

    feat: implement touch sensor

--- src/main.c
+++ src/main.c
@@ -3,41 +3,85 @@
 #include <avr/io.h>
 #include <avr/wdt.h>
 #include <usbdrv.h>
+#include <stdbool.h>
 
-#define KEY_A 0x04
+#define LED_PIN 2
+#define BTN_PIN 3
+#define TOUCH_IN_PIN 6
+#define TOUCH_OUT_PIN 7
+
+#define BIT(x) (1 << (x))
+
+#define TOUCH_THRES 15
 
 uint8_t reportBuffer[1];
 
 int main() {
   // setup
-  DDRC = 0b10;
-  PORTC = 0;
+  DDRA |= BIT(LED_PIN) | BIT(TOUCH_OUT_PIN);
+  PORTA = 0;
 
   usbInit();
   usbDeviceConnect();
   sei();
 
-  // main
-  uint8_t v = 0;
+  // key to send at next interrupt
+  uint8_t k = 0;
+
+  uint8_t h = 0; // hysteresis
+  bool v = 0; // current btn state
+  bool v_last = 0; // last btn state
+
+  uint8_t pulser_cnt = 0;
 
   while (1) {
     wdt_reset();
     usbPoll();
 
-    if (PINC & 0b1) {
-      PORTC |= 0b10;
-    } else {
-      PORTC &= ~0b10;
+    if (k && usbInterruptIsReady()) {
+      if (k == 0xff) k = 0;
+
+      reportBuffer[0] = k;
+      usbSetInterrupt(reportBuffer, sizeof(reportBuffer));
+      k = k ? 0xff : 0; // release
     }
 
-    if ((PINC & 0b1) != v) {
-      v = PINC & 0b1;
 
-      if (usbInterruptIsReady()) {
-        reportBuffer[0] = v ? KEY_A : 0;
-        usbSetInterrupt(reportBuffer, sizeof(reportBuffer));
+    // touch sensor
+    // check every 16th time
+    ++pulser_cnt; pulser_cnt &= 0x0f;
+    if (pulser_cnt == 0) {
+      if (!(PORTA & BIT(TOUCH_OUT_PIN))) {
+        uint8_t rise_time_cnt = 1;
+        PORTA |= BIT(TOUCH_OUT_PIN);
+        while (rise_time_cnt <= TOUCH_THRES && !(PINA & BIT(TOUCH_IN_PIN))) {
+          ++rise_time_cnt;
+        }
+        rise_time_cnt = rise_time_cnt > TOUCH_THRES;
+
+        // hyseteresis
+        if (v != rise_time_cnt) {
+          ++h;
+          if (h > 20) {
+            h = 0;
+            v = rise_time_cnt;
+          }
+        } else {
+          h = 0;
+        }
+
+      } else {
+        PORTA &= ~BIT(TOUCH_OUT_PIN);
       }
     }
+
+    if (v) PORTA |= BIT(LED_PIN);
+    else PORTA &= ~BIT(LED_PIN);
+
+    if (v && !v_last) {
+      k = 0x04; // A
+    }
+    v_last = v;
   }
 }
 
@@ -49,7 +93,6 @@ usbMsgLen_t usbFunctionSetup(uchar data[8]) {
   if((rq->bmRequestType & USBRQ_TYPE_MASK) == USBRQ_TYPE_CLASS) {
     switch(rq->bRequest) {
       case USBRQ_HID_GET_REPORT:
-        reportBuffer[0] = (PINC & 0b1) ? KEY_A : 0;
         usbMsgPtr = reportBuffer;
         return sizeof(reportBuffer);
       case USBRQ_HID_GET_IDLE:
